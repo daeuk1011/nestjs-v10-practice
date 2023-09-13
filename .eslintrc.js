56d45e998843d6e6531c2c9df12c5f001e034121
daeuk1011 <istpdeveloper@gmail.com>
2023-09-13 17:19:07 +0900

    chore(lint): lint 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -21,5 +21,53 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+
+    'prettier/prettier': ['error', { printWidth: 120 }],
+    'import/no-unresolved': ['error'],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'index'],
+        pathGroups: [
+          {
+            pattern: '@nestjs/**',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@apps/**',
+            group: 'index',
+            position: 'before',
+          },
+          {
+            pattern: '@libs/**',
+            group: 'index',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['nestjs'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'import/prefer-default-export': 'off',
+  },
+
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.js'],
+    },
+    'import/extensions': ['.ts', '.js'],
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+      node: {
+        extensions: ['.ts', '.js'],
+      },
+    },
   },
 };
